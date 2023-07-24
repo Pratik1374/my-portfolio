8c984602e4ac2c11301728759b29ed0f69a29071
Pratik <pratik1374@gmail.com>
2023-07-24 20:05:32 +0530

    Updated Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,14 +33,19 @@ const Home = () => {
       className="my-bg-dark-gradient text-white relative w-full h-[750px] md:h-[600px] mx-auto py-20 flex flex-wrap items-center "
     >
       <div className="w-full h-1/2 md:w-1/2 flex justify-center items-center">
-        <img src={profileImg} alt="" className="w-[300px] h-[300px]" />
+        <div className="border-animation-div w-[250px] h-[250px] lg:w-[350px] lg:h-[350px]">
+        <img src={profileImg} alt="" className="w-[230px] h-[230px] lg:w-[330px] lg:h-[330px] bg-white" />
+        </div>
       </div>
       <div className="w-full h-1/2 md:w-1/2 flex">
         <div className="flex flex-col ">
-          <h1 className="mx-[10px] font-black text-white lg:text-[40px] sm:text-[40px] xs:text-[50px] text-[40px] flex flex-col ">
+          <h1 className="mx-[20px] font-black text-white lg:text-[40px] sm:text-[40px] xs:text-[50px] text-[40px] flex flex-col ">
             <span className="text-[30px] lg:text-[40px]">Hi, I'm </span>
             <div>
-              <span className="text-[#915EFF] text-[30px] lg:text-[40px] drop-shadow-lg  shadow-white" style={{textShadow: "1px 1px 19px rgba(143, 53, 255, 1)"}}>
+              <span
+                className="text-[#915EFF] text-[30px] lg:text-[40px] drop-shadow-lg  shadow-white"
+                style={{ textShadow: "1px 1px 19px rgba(143, 53, 255, 1)" }}
+              >
                 {text}
               </span>
               <Cursor
@@ -58,17 +63,23 @@ const Home = () => {
               ease: "linear",
             }}
             initial={{ opacity: 0, scale: 0.5 }}
-            className=" p-2 my-tagline-bg mx-2 mt-4 rounded-md"
+            className=" p-2 my-tagline-bg mx-[20px] mt-4 rounded-md"
           >
             "Coding is my canvas, and technology is my palette. Through lines of
             code and pixels of innovation, I paint immersive digital worlds that
             inspire, engage, and push the boundaries of what's possible."
           </motion.p>
           <div className=" flex justify-center pt-10 lg:pt-20">
-          <button className="bg-cyan-400 text-black font-semibold p-2 rounded-lg  hover:scale-105" onClick={handleDownload}>Download Resume</button>
+            <button
+              className="bg-cyan-400 text-black font-semibold p-2 rounded-lg  hover:scale-105"
+              onClick={handleDownload}
+            >
+              Download Resume
+            </button>
           </div>
         </div>
       </div>
+      
     </section>
   );
 };
